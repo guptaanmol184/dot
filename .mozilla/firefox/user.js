85e607851c1732f4b6a1ea04cc16c5cf779c17b8
Anmol Gupta <guptaanmol184@gmail.com>
2019-01-09 22:49:36 +0530

    tweak scroll settings

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -8,7 +8,7 @@ user_pref("general.smoothScroll.other.durationMinMS", 125);
 user_pref("general.smoothScroll.pages.durationMaxMS", 125);
 user_pref("general.smoothScroll.pages.durationMinMS", 125);
 
-user_pref("mousewheel.min_line_scroll_amount", 30);
+user_pref("mousewheel.min_line_scroll_amount", 15);
 user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
 user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
 user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
